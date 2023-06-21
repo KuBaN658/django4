8a1910f6fe02e59c4d1699da7f09a98f9fc32371
KuBAN23_ <garkovski_dmitri@mail.ru>
2023-06-21 10:56:20 +0300

    постраничная разбивка blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import (Paginator,
+                                   EmptyPage,
+                                   PageNotAnInteger)
 
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   context={
@@ -12,10 +23,14 @@ def post_list(request):
                   })
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day
+                             )
     return render(request,
                   'blog/post/detail.html',
                   context={
