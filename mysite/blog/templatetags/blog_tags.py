240b9e235134724fc7a9b5b9a817003f9df700e4
KuBaN23_ <garkovski_dmitri@mail.ru>
2023-06-29 20:11:47 +0300

    самые обсуждаемые статьи

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 
 register = template.Library()
@@ -16,3 +17,10 @@ def show_latest_posts(count=3):
     return {
         'latest_posts': latest_posts
     }
+
+
+@register.simple_tag
+def get_most_commented_posts(count=3):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
