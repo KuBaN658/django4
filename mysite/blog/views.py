a328b8018fc0562a5bc4f258874e732412ab7f3f
KuBaN23_ <garkovski_dmitri@mail.ru>
2023-06-29 15:04:22 +0300

    показ похожих статей

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -54,12 +55,21 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
+
+    #  список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   context={
                       'post': post,
                       'comments': comments,
                       'form': form,
+                      'similar_posts': similar_posts,
                   })
 
 
